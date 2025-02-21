c8f8739033e2d469c950a47543197572f4afbfcf
shashankb-cc <shashank.b@codecraft.co.in>
2025-02-21 15:21:42 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Shashank B on 21/02/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
